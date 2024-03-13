4cb2064ae4ac32c56fd409495df57b39d9652d59
CappelliLeonardo <146942911+CappelliLeonardo@users.noreply.github.com>
2024-03-13 16:20:21 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    },  
+    methods: {
+
+    },
+    mounted(){
+
+    },
+}
+</script>
+
+<template>
+    <h1>
+        Mia App
+    </h1>
+
+    <AppHeader />
+
+    <AppMain />
+
+    <AppFooter />
+</template>
+
+<style lang="scss">
+@use "assets/scss/main" as *;
+@import "assets/scss/partials/reset";
+</style>
